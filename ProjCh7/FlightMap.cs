3284ace5e2ca691beceba6b2c28895aeacd8765d
Garth Sorenson <garth.sorenson@snow.edu>
2022-10-10 22:32:19 -0600

    Setup FlightMapInterface, an empty FlightMap class, and empty City class, and have most of the driver written.

--- ProjCh7/FlightMap.cs
+++ ProjCh7/FlightMap.cs
@@ -2,35 +2,11 @@
 
 namespace ProjCh7
 {
-    internal interface FlightMap
+    internal class FlightMap : FlightMapInterface
     {
-        //public FlightMap();
-        // Creates an empty flight map
-
-        public void read(string CityFileName, string flightFileName);
+        public void read(string CityFileName, string flightFileName)
+        { }
         // Read flight information into the flight map
-
-        public void display();
-        // Displays flight information
-
-        public void displayAllCities();
-        // Displays the names of all cities that HPAir serves
-
-        public void displayAdjacentCities(City aCity);
-        // Displays all cities that are adjacent to a given city.
-
-        public void markVisited(City aCity);
-        // Marks a city as visited
-
-        public void unvisitAll();
-        // Clears marks on all cities
-
-        public bool isVisited(City aCity);
-        // Determines whether a city was visited.
-
-        public void insertAdjacent(City aCity, City adjCity);
-        // Inserts a city adjacent to another city in a flight map.
-
 
     }
 }
--- ProjCh7/Program.cs
+++ ProjCh7/Program.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿// Program.cs contains a driver for the HP Airlines program utilizing a FlightMap class
+// Written by Garth Sorenson
+// 10 Oct 2022
+
+using System;
+using System.IO;
+
 
 namespace ProjCh7
 {
     internal class Program
     {
+        // Main is the beginning of the HP Airlines program.
+        // It gets a City filename, a Flight filename, and a Request filename
+        // from the user.  A FlightMap is created from the City file and the
+        // Flight file.  Then each request in the Request file is handled.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string cityFilename, flightFilename, requestFilename;
+            FlightMap map;
+
+            // display greeting
+            Console.WriteLine("HP Airlines!\n");
+            Console.WriteLine("A flight map is generated based on the cities served listed in a City file");
+            Console.WriteLine("and the direct flights listed in a Flight file.");
+            Console.WriteLine("Then all requests listed in a Request file will be considered.");
+
+            // get City filename
+            cityFilename = GetValidFilename("City");
+
+            // get Flight filename
+            flightFilename = GetValidFilename("Flight");
+
+            // create flight map based on City file and Flight file
+            map = new FlightMap();
+            map.read(cityFilename, flightFilename);
+
+            // get Request filename, open file, and handle each request
+            requestFilename = GetValidFilename("Request");
+
+        }
+
+        // Gets and returns a valid file's name from the user.
+        public static string GetValidFilename(string which)
+        {
+            string filename;
+ 
+            Console.Write($"\nEnter {which} filename: ");
+            filename = Console.ReadLine().Trim();
+            while (!File.Exists(filename))
+            {
+                Console.WriteLine(filename + " does not exist.  Try again.");
+                Console.Write($"Enter {which} filename: ");
+                filename = Console.ReadLine().Trim();
+            }
+
+            return filename;
         }
     }
 }
